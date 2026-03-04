293b5e3c4e1d3b281d3fc8b8550477588bb750dc
Aakanksha <204729565+BlissfulAlways@users.noreply.github.com>
2026-03-04 18:27:44 +0000

    Team 01 : Project foundation, Spring Boot project structure and project layout

---
+++ backend/src/main/java/com/collabspace/CollabSpace/CollabSpaceApplication.java
@@ -0,0 +1,13 @@
+package com.collabspace.CollabSpace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollabSpaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollabSpaceApplication.class, args);
+	}
+
+}
