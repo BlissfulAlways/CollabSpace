1e52b46949331f987ae90a9349d469d03b7c0e8f
Aakanksha <204729565+BlissfulAlways@users.noreply.github.com>
2026-03-10 11:51:57 +0000

    zustand code

---
+++ frontend/src/stores/useUserStore.js
@@ -0,0 +1,8 @@
+import {create} from 'zustand'
+
+const useUserStore = create((set) => ({
+    user: null,
+    token: null,
+    setUser: (user, token) => set({ user,token }),
+    logout: () => set({ user: null, token: null})
+}))
